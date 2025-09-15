aad4d026133910da6f18dab2e5f9de27de334916
yashdeep0904 <yashdeep0904@gmail.com>
2025-09-15 21:59:41 +0530

    Create variables.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 13234
+let accountEmail = "yash0904@gmai.com"
+var accountPassword = "12345"
+accountCity = "Faridabad"
+ let accountState
+// accountId = 23232
+
+accountEmail = "yash@gmail.com"
+accountPassword = "yash123"
+accountCity = "Rohtak"
+// console.log(accountId)
+// console.log(accountCity);
+// console.log(accountEmail);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
