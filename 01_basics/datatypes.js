07de29df4a39e5af68bb2a314e0ee78d78f6660d
yashdeep0904 <yashdeep0904@gmail.com>
2025-09-15 22:53:43 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+// alert(3+3) //we are using node js not broweser
+
+"use strict" ;//treat all js code as new version
+
+// data types
+
+let name =  "Yashdeep"
+let age = 21
+let userLoggedIn = true
+
+// string => " "
+// bigint 
+// number => 2 raise to power 53
+// boolean => true or false
+// null => standalone value or datatype
+// undefined => abhi value assign nhi hui h
+// symbol => to find uniqueness
+
+console.log(typeof 21)
+console.log(typeof null,undefined)
