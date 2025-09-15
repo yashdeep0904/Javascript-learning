f6bdee0ff103df236bfb91dfab712f4a7ce8e762
yashdeep0904 <yashdeep0904@gmail.com>
2025-09-16 02:20:20 +0530

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,20 @@
+console.log(1<2)
+console.log(2>1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2">1)
+console.log("02">10)
+
+console.log("comparing using null datatype ")
+console.log(null>0)
+console.log(null >= 0)
+console.log(null == 0)
+
+console.log("comparing with ==")
+console.log("it shows true because it does not checks for the datatype")
+console.log("2" == 2)
+
+console.log("comparing with ===")
+console.log("it shows false because it also checks for the datatype")
+console.log("2" === 2)
