a3c869e6b6cbbfd8f4401bc05d435323cff9838b
yashdeep0904 <yashdeep0904@gmail.com>
2025-10-19 05:06:52 +0530

    invoked functions

---
+++ 01_basics/invoked_functions.js
@@ -0,0 +1,12 @@
+
+(function one(){
+    console.log("DB connected")
+})();    
+
+(()=>{
+    console.log("DB connected again")
+})();
+
+((name)=>{
+    console.log(`DB connected again ${name}`)
+})('Yashdeep')
